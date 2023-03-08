e6f487e5f8049717b1361c7f9c9e2be913b402cb
Derek Ronske <dronske@iu.edu>
2023-03-08 14:46:29 -0500

    string calculator exercise

---
+++ stringcalculator-wed8/StringCalculator.cs
@@ -0,0 +1,29 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculator
+{
+
+    public int Add(string numbers)
+    {
+        if (String.IsNullOrEmpty(numbers)) return 0;
+        var arrayNumbers = numbers.Split(',','\n');
+        try
+        {
+            int.Parse(arrayNumbers[0]);
+        } catch (FormatException)
+        {
+            var custom = arrayNumbers[0][2];
+            arrayNumbers = numbers.Split(',','\n',custom);
+            arrayNumbers = arrayNumbers[2..arrayNumbers.Length];
+        }
+        var sum = 0;
+        for (int i = 0; i < arrayNumbers.Length; i++)
+        {
+            var currentNumber = int.Parse(arrayNumbers[i]);
+            if (currentNumber < 0) throw new InvalidOperationException();
+            sum += int.Parse(arrayNumbers[i]);
+        }
+        return sum;
+    }
+}
---
+++ stringcalculator-wed8/StringCalculatorTests.cs
@@ -0,0 +1,79 @@
+﻿
+namespace StringCalculator;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new StringCalculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("16", 16)]
+    [InlineData("3", 3)]
+    public void SingleDigitReturnsSelf(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("16,3", 19)]
+    [InlineData("3,4", 7)]
+    [InlineData("10,5", 15)]
+    public void TwoDigitsReturnsSum(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("16,3,4", 23)]
+    [InlineData("3,4,5", 12)]
+    [InlineData("10,5,101", 116)]
+    public void UnlimitedDigitsReturnsSum(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("16,3\n4", 23)]
+    [InlineData("3\n4\n5", 12)]
+    [InlineData("10,5,101\n5", 121)]
+    public void AllowNewLinesAsDelimiter(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("//;\n16,3;4", 23)]
+    [InlineData("//d\n3d4d5", 12)]
+    [InlineData("// \n10 5,101 5", 121)]
+    public void AllowCustomDelimiters(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        var result = calculator.Add(input);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("//;\n16,3;-4", 15)]
+    [InlineData("//d\n3d-4d5", 12)]
+    [InlineData("// \n-10 -5,-101 -5", 121)]
+    public void NegativesNotAllowed(string input, int expected)
+    {
+        var calculator = new StringCalculator();
+        Assert.Throws<InvalidOperationException>(() => calculator.Add(input));
+    }
+}
