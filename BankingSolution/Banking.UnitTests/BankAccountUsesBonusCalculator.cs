c2ab10d3e2ac9d6d336eb73b78d96b406e81c6b8
Derek Ronske <dronske@iu.edu>
2023-03-09 12:32:51 -0500

    moq

--- BankingSolution/Banking.UnitTests/BankAccountUsesBonusCalculator.cs
+++ BankingSolution/Banking.UnitTests/BankAccountUsesBonusCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using Banking.Domain;
 using Banking.UnitTests.TestDoubles;
+using Moq;
 
 namespace Banking.UnitTests;
 
@@ -12,6 +13,21 @@ public class BankAccountUsesBonusCalculator
 
         bankAccount.Deposit(212.83M);
 
+        Assert.Equal(5000M + 212.83M + 12M, bankAccount.GetBalance());
+    }
+
+    [Fact]
+    public void IntegratesWithBonusCalculatorStubbedObject()
+    {
+        // Given
+        var stubbedBonusCalculator = new Mock<ICalculateBonuses>();
+        var bankAccount = new BankAccount(stubbedBonusCalculator.Object);
+        stubbedBonusCalculator.Setup(dil => dil.CalculateBankAccountDepositBonusFor(bankAccount.GetBalance(), 212.83M)).Returns(12M);
+
+        // When
+        bankAccount.Deposit(212.83M);
+
+        // Then
         Assert.Equal(5000M + 212.83M + 12M, bankAccount.GetBalance());
     }
 }
