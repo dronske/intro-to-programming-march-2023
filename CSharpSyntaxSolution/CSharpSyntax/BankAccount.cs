6fd6643a27e3b970cdfe3100d1f552596fb914cc
Derek Ronske <dronske@iu.edu>
2023-03-10 15:50:39 -0500

    fri

---
+++ CSharpSyntaxSolution/CSharpSyntax/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpSyntax;
+
+public class BankAccount
+{
+    private string _accountNumber = string.Empty;
+    private string _firstName = string.Empty;
+    
+    internal BankAccount(string accountNumber)
+    {
+        _accountNumber = accountNumber;
+    }
+
+    public string GetAccountNumber()
+    {
+        return _accountNumber;
+    }
+
+    public string GetFirstName()
+    {
+        return _firstName;
+    }
+
+    public void SetFirstName(string name)
+    { 
+        _firstName = name;
+    }
+}
+
+public class AccountManager
+{
+    public BankAccount GetAccountById(string accountId)
+    {
+        return new BankAccount(accountId);
+    }
+}
---
+++ CSharpSyntaxSolution/CSharpSyntax/WorkingWithMethods.cs
@@ -0,0 +1,42 @@
+﻿namespace CSharpSyntax;
+
+public class WorkingWithMethods
+{
+    [Fact]
+    public void WorkingWithStuff()
+    {
+        var o1 = new Stuff();
+        o1.DoIt();
+
+        var o2 = new Stuff();
+        o2.DoIt();
+
+        Assert.Equal(19, o2.GetX());
+        Assert.Equal(19, o1.GetX());
+
+        o2.DoIt();
+        o2.DoIt();
+
+        Assert.Equal(19, o1.GetX());
+        Assert.Equal(17, o2.GetX());
+    }
+
+    [Fact]
+    public void HeapStuff()
+    {
+        var s1 = new Stuff();
+        var s2 = s1;
+        s2.SetX(108);
+
+        Assert.Equal(108, s1.GetX());
+    }
+
+    [Fact]
+    public void SideEffects()
+    {
+        var s3 = new Stuff();
+        StuffUtilities.DoSomeStuff(s3);
+
+        Assert.Equal(500, s3.GetX());
+    }
+}
