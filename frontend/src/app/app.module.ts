3b3507e2db7be9c8045d6e209ffd39b2ae6893be
Derek Ronske <dronske@iu.edu>
2023-03-13 12:30:07 -0400

    moremoremore

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MastheadComponent } from './components/masthead/masthead.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { SupportComponent } from './components/support/support.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MastheadComponent
+    MastheadComponent,
+    DashboardComponent,
+    NavigationComponent,
+    SupportComponent
   ],
   imports: [
     BrowserModule,
