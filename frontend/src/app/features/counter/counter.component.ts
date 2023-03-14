b38b93f6676d049fb77803dbe1e3a16051cd4c59
Derek Ronske <dronske@iu.edu>
2023-03-14 12:34:29 -0400

    settings

--- frontend/src/app/features/counter/counter.component.ts
+++ frontend/src/app/features/counter/counter.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
+  current = 0;
 
+  increment() {
+    this.current++;
+  }
+
+  decrement() {
+    this.current--;
+  }
 }
--- frontend/src/app/features/counter/counter.module.ts
+++ frontend/src/app/features/counter/counter.module.ts
@@ -1,7 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { CounterComponent } from './counter.component';
-import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
