6c6c6fa3c4bdf53d5f38c9ac7efcc6204bb14811
Derek Ronske <dronske@iu.edu>
2023-03-14 15:21:45 -0400

    works

--- frontend/src/app/features/counter/counter.component.ts
+++ frontend/src/app/features/counter/counter.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCounterCurrent } from './state';
+import { counterEvents } from './state/actions/counter.actions';
 
 @Component({
   selector: 'app-counter',
@@ -6,13 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent {
-  current = 0;
+  current$ = this.store.select(selectCounterCurrent);
+
+  constructor(private readonly store:Store) {}
 
   increment() {
-    this.current++;
+    // this.current++;
+    this.store.dispatch(counterEvents.incrementButtonClicked());
   }
 
   decrement() {
-    this.current--;
+    // this.current--;
+    this.store.dispatch(counterEvents.decrementButtonClicked());
   }
 }
--- frontend/src/app/features/counter/state/reducers/counter.reducer.ts
+++ frontend/src/app/features/counter/state/reducers/counter.reducer.ts
@@ -1,4 +1,5 @@
-import { createReducer } from "@ngrx/store";
+import { createReducer, on } from "@ngrx/store";
+import { counterEvents } from "../actions/counter.actions";
 
 // Describe it for TypeScript
 export interface CounterState {
@@ -7,7 +8,12 @@ export interface CounterState {
 
 // What is the initial state on application start?
 const initialState:CounterState = {
-    current: 42,
+    current: 0,
 };
 
-export const reducer = createReducer(initialState);
+// Here's where we decide that if an action happens, does that mean a new state is created?
+// Access to current state, as well as action that just happened, and use that to create a new state (if we want)
+export const reducer = createReducer(initialState,
+    on(counterEvents.incrementButtonClicked, (currentState: CounterState) => ({...currentState, current: currentState.current + 1})),
+    on(counterEvents.decrementButtonClicked, (s) => ({...s, current: s.current - 1}))
+);
