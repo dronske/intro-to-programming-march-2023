606e3c3a85be796955172e531c4518136c4cc4bb
Derek Ronske <dronske@iu.edu>
2023-03-15 12:25:10 -0400

    setting up api

--- frontend/src/app/features/counter/state/effects/counter.effects.ts
+++ frontend/src/app/features/counter/state/effects/counter.effects.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType, concatLatestFrom } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { filter, map, tap } from 'rxjs';
+import { z } from 'zod';
+import { catchError, filter, map, of, tap } from 'rxjs';
 import { selectCounterBranch } from '..';
 import { counterCommands, counterDocuments, counterEvents } from '../actions/counter.actions';
 import { CounterState } from '../reducers/counter.reducer';
 
 @Injectable()
 export class CounterEffects {
+
+    private readonly CountDataSchema = z.object({
+        current: z.number(),
+        by: z.union([
+            z.literal(1),
+            z.literal(3),
+            z.literal(5)
+        ])
+    });
 
     // logItAll$ = createEffect(
     //     () => {
@@ -35,7 +45,13 @@ export class CounterEffects {
             map(() => localStorage.getItem('counter-state')),
             filter(storedValue => storedValue !== null),
             map(theString => JSON.parse(theString!) as CounterState),
-            map(counterState => counterDocuments.counterState({payload: counterState})) // the action to send to the state
+            map(susObject => this.CountDataSchema.parse(susObject)),
+            map(counterState => counterDocuments.counterState({payload: counterState})), // the action to send to the state
+            catchError(() => {
+                console.log("Uh oh");
+                localStorage.clear();
+                return of({type: 'Localstorage Error'});
+            })   
         );
     }
     );
