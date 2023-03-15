fbc71910a2ab07a715c1fe0a6b02afe591ce0420
Derek Ronske <dronske@iu.edu>
2023-03-15 14:46:52 -0400

    api

---
+++ LearningResourcesSolution/LearningResourcesApi/Controllers/LearningResourcesController.cs
@@ -0,0 +1,22 @@
+﻿namespace LearningResourcesApi.Controllers;
+
+public class LearningResourcesController : ControllerBase
+{
+    private readonly LearningResourcesDataContext _context;
+
+    public LearningResourcesController(LearningResourcesDataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("/learning-resources")]
+    public async Task<ActionResult<LearningResourcesResponse>> GetLearningResources()
+    {
+        var data = await _context.LearningResources
+            .Where(item => item.WhenRemoved == null)
+            .Select(item => new LearningResourcesSummaryItem(item.Id.ToString(), item.Name, item.Description, item.Link))
+            .ToListAsync();
+        var response = new LearningResourcesResponse(data);
+        return Ok(response);
+    }
+}
---
+++ LearningResourcesSolution/LearningResourcesApi/Models/LearningResourcesModels.cs
@@ -0,0 +1,7 @@
+﻿namespace LearningResourcesApi.Models;
+
+public record LearningResourcesSummaryItem(
+    string Id, string Name, string Description, string Link);
+
+public record LearningResourcesResponse(List<LearningResourcesSummaryItem> Data);
+
